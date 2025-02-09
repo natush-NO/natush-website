e169ae2383b15db06ba055f1e1e2e05e4b4395fb
natush <osmannatali22@gmail.com>
2025-02-09 21:06:57 +0100

    Fixing the background path for the body

--- styles.js
+++ styles.js
@@ -52,7 +52,7 @@ body {
   line-height: 1;
   font-weight: 600;
   background-color: #07171f;
-  background-image: url("public/background/background.jpg");
+  background-image: url("/background/background.jpg");
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
